9b27adcbac85f8c9514d3bdcd20ecde4d6cf9470
slightlycyborg <slightlycyborg@gmail.com>
2016-06-19 08:02:01 +0000

    css progress on /pipeline

--- project.clj
+++ project.clj
@@ -20,7 +20,8 @@
                  [com.taoensso/encore "2.56.1"]
                  [jayq "2.5.4"]
                  [cljs-ajax "0.5.5"]
-                 [ring-transit "0.1.5"]]
+                 [ring-transit "0.1.5"]
+                 [garden "1.3.2"]]
   :plugins [[lein-figwheel "0.5.4-3"]]
   :cljsbuild {
     :builds [{:id "clj-eyes" 
