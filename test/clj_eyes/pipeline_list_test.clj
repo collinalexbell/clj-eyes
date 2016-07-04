addff63e872a03f5dc2d91c6b61ab8bc7f143aae
slightlycyborg <slightlycyborg@gmail.com>
2016-07-04 06:51:31 +0000

    updated pipeline_test.clj for recent refactor

---
+++ test/clj_eyes/pipeline_list_test.clj
@@ -0,0 +1,31 @@
+(ns clj-eyes.pipeline-list-test
+  (:use clojure.test)
+  (:require [clj-eyes.pipeline :as pipeline]))
+
+(deftest mat-eq?
+  (let [mat-1 (Imgcodecs/imread "resources/public/imgs/test-pattern.png")
+        mat-2 (Imgcodecs/imread "resources/public/imgs/test-pattern.png")
+        mat-3 (pipeline/mat-with-size-of mat-1)
+        mat-4 (pipeline/mat-with-size-of mat-1)]
+    (is (pipeline/mat-eq? mat-1 mat-2))
+    ;Blur the image and test for equality
+    (Imgproc/GaussianBlur mat-1 mat-3 (Size. 3 3) 0.0)
+    (Imgproc/GaussianBlur mat-1 mat-4 (Size. 3 3) 0.0)
+    (is (not (pipeline/mat-eq? mat-1 mat-3)))
+    (is (pipeline/mat-eq? mat-3 mat-4))))
+
+(deftest load-new-source
+  ;load new sorce requires an atom representing the pipeline
+  (let [pipeline-list 
+        (pipeline/load-new-source "resources/imgs/tomato_plants.jpg" 1 {})]
+     (is
+      (contains? (get pipeline-list 1 nil) :pipeline-source-img))
+     ;;Then assert that the frame contains what a frame should contain an id and a sourc frame
+     (let [src-frame (:pipeline-source-img (get pipeline-list 1 nil))]
+       (is (contains? src-frame :id))
+       (is (contains? src-frame :source-frame)))))
+
+(deftest fetch-webp
+  (let [webp-struct (pipeline/fetch-webp (pipeline/load-new-source "resources/imgs/tomato_plants.jpg" 1 {}) 1 :pipeline-source-img)]
+    (is (= (type (:value webp-struct)) org.opencv.core.MatOfByte))))
+
--- test/clj_eyes/pipeline_test.clj
+++ test/clj_eyes/pipeline_test.clj
@@ -1,52 +1,26 @@
 (ns clj-eyes.pipeline-test
   (:use clojure.test)
-  (:require [clj-eyes.cv-pipeline :as pipeline]))
+  (:require [clj-eyes.pipeline :as pipeline]))
 
 (import '[org.opencv.core MatOfInt MatOfByte Mat CvType Size]
         '[org.opencv.imgcodecs Imgcodecs]
         '[org.opencv.imgproc Imgproc])
 
-(deftest load-new-source
-  ;load new sorce requires an atom representing the pipeline
-  (let [pipeline-list 
-        (pipeline/load-new-source "resources/imgs/tomato_plants.jpg" 1 {})]
-     (is
-      (contains? (get pipeline-list 1 nil) :pipeline-source-img))
-     ;;Then assert that the frame contains what a frame should contain an id and a sourc frame
-     (let [src-frame (:pipeline-source-img (get pipeline-list 1 nil))]
-       (is (contains? src-frame :id))
-       (is (contains? src-frame :source-frame)))))
-
-(deftest fetch-webp
-  (let [webp-struct (pipeline/fetch-webp (pipeline/load-new-source "resources/imgs/tomato_plants.jpg" 1 {}) 1 :pipeline-source-img)]
-    (is (= (type (:value webp-struct)) org.opencv.core.MatOfByte))))
-
 (deftest generate-transform-id
-  (let [filter-id1 (pipeline/generate-transform-id {1 {:src-frame {}}} 1)
-        filter-id2 (pipeline/generate-transform-id {1 {:src-frame {} :transform1 {}}} 1)]
-    (is (= filter-id1 :transformation1))
-    (is (= filter-id2 :transformation2))))
-
-(deftest mat-eq?
-  (let [mat-1 (Imgcodecs/imread "resources/public/imgs/test-pattern.png")
-        mat-2 (Imgcodecs/imread "resources/public/imgs/test-pattern.png")
-        mat-3 (pipeline/mat-with-size-of mat-1)
-        mat-4 (pipeline/mat-with-size-of mat-1)]
-    (is (pipeline/mat-eq? mat-1 mat-2))
-    ;Blur the image and test for equality
-    (Imgproc/GaussianBlur mat-1 mat-3 (Size. 3 3) 0.0)
-    (Imgproc/GaussianBlur mat-1 mat-4 (Size. 3 3) 0.0)
-    (is (not (pipeline/mat-eq? mat-1 mat-3)))
-    (is (pipeline/mat-eq? mat-3 mat-4))))
+  (let [filter-id1 (pipeline/generate-transform-id {:tree {:src-frame {}} :meta-data {:frame-no 0}})
+        filter-id2 (pipeline/generate-transform-id {:tree {:src-frame {} :transform1 {}} :meta-data {:frame-no 1}})]
+    (is (= filter-id1 :transformation0))
+    (is (= filter-id2 :transformation1))))
 
 (deftest find-by-parent
   (is (= (pipeline/find-by-parent
           :transformation1
-          {:transformation1
-           {:source-frame :pipeline-source-img}
-           :transformation2
-           {:source-frame :transformation1
-            :data :derp}})
+          {:tree
+           {:transformation1
+            {:source-frame :pipeline-source-img}
+            :transformation2
+            {:source-frame :transformation1
+             :data :derp}}})
          [{:source-frame :transformation1 :data :derp}])))
 
 (deftest assoc-all-in-list
@@ -76,7 +50,7 @@
        :item3 {:parent :item1 :text :updated}
        :item4 {:parent :item2 :text :updated}}
 
-      update-fn #(assoc %1 :text :updated)]
+      update-fn (fn [arg1 arg2] (assoc arg1 :text :updated))]
 
    (is (= tree-updated-from-item2 (pipeline/update-tree-recursively
                                    source-tree
@@ -89,6 +63,8 @@
                                    :parent
                                    :item1
                                    update-fn)))))
+
+
 
 (run-tests)
 
