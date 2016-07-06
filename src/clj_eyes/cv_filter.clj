bfe07e53b05a5a652fa1f6f7c767209a5f2cba3b
slightlycyborg <slightlycyborg@gmail.com>
2016-07-06 12:11:08 +0000

    added all of the filters that do not require extra client side features such as Mat based param selection and Point based param selection

--- src/clj_eyes/cv_filter.clj
+++ src/clj_eyes/cv_filter.clj
@@ -19,6 +19,32 @@
                             (read-string sigma-space)
                             (read-string border-type))))
 
+(defn sobel
+  ([src dest dx dy]
+   (Imgproc/Sobel src dest
+                  -1
+                  (read-string dx)
+                  (read-string dy)))
+
+  ([src dest dx dy ksize scale delta]
+   (Imgproc/Sobel src dest
+                  -1
+                  (read-string dx)
+                  (read-string dy)
+                  (read-string ksize)
+                  (read-string scale)
+                  (read-string delta)))
+
+  ([src dest dx dy ksize scale delta border-type]
+   (Imgproc/Sobel src dest
+                  -1
+                  (read-string dx)
+                  (read-string dy)
+                  (read-string ksize)
+                  (read-string scale)
+                  (read-string delta)
+                  (read-string border-type))))
+
 (defn gaussian-blur
   ([src dest kernal-height kernal-width standard-deviation-x]
    (Imgproc/GaussianBlur src dest (Size.
@@ -47,6 +73,25 @@
                   (read-string aperture-size)
                   (read-string l2-gradient))))
 
+(defn median-blur
+  [src dest ksize]
+  (Imgproc/medianBlur src dest (read-string ksize)))
+
+(defn box-filter
+  [src dest kwidth kheight]
+  (Imgproc/boxFilter src dest -1
+                     (Size.
+                      (read-string kwidth)
+                      (read-string kheight))))
+
+(defn blur 
+  [src dest kwidth kheight]
+  (Imgproc/boxFilter src dest -1
+                     (Size.
+                      (read-string kwidth)
+                      (read-string kheight))))
+
+
 (defn gen-filter-param
   "Generates a map that defines a filter-param"
   [type label default option-group & [additional-arg-map]]
@@ -57,6 +102,15 @@
     :option-group option-group
     :obligation (if (< option-group 1) :manditory :optional)}
    additional-arg-map))
+
+(defn border-type-param [option-group]
+  (gen-filter-param :select "Border Type" "4" option-group
+                    {:options [{:label "Constant"    :value 0}
+                               {:label "Replicate"   :value 1}
+                               {:label "Reflect"     :value 2}
+                               {:label "Wrap"        :value 3}
+                               {:label "Reflect 101" :value 4}
+                               {:label "Isolated"    :value 16}]}))
 
 (def filter-params
  {:canny
@@ -75,21 +129,39 @@
   [(gen-filter-param :range "Pixel Neighborhood Diameter" "1" 0 {:step 1 :min 1 :max 10})
    (gen-filter-param :range "Sigma Color" "0" 0 {:step 0.2 :min 0 :max 400})
    (gen-filter-param :range "Sigma Space" "0" 0 {:step 0.2 :min 0 :max 400})
-   (gen-filter-param :select "Border Type" "4" 1 {:options [{:label "Constant"    :value 0}
-                                                            {:label "Replicate"   :value 1}
-                                                            {:label "Reflect"     :value 2}
-                                                            {:label "Wrap"        :value 3}
-                                                            {:label "Reflect 101" :value 4}
-                                                            {:label "Isolated"    :value 16}]})]})
+   (border-type-param 1)]
+
+  :sobel
+  [(gen-filter-param :range "X Derivative Order" "1" 0 {:step 1 :min 0 :max 3})
+   (gen-filter-param :range "Y Derivative Order" "0" 0 {:step 1 :min 0 :max 3})
+   (gen-filter-param :range "Kernal Size" "3" 1 {:step 2 :min 1 :max 7})
+   (gen-filter-param :range "Scale Factor" "1" 1 {:min 1 :max 5})
+   (gen-filter-param :range "Delta" "0" 1 {:min 0 :max 100})
+   (border-type-param 2)]
+
+  :median-blur
+  [(gen-filter-param :range "Kernal Size" "3" 0 {:step 2 :min 3 :max 5})]
+
+  :box-filter
+  [(gen-filter-param :range "Kernal Width" "1" 0 {:min 1 :max 50})
+   (gen-filter-param :range "Kernal Height" "1" 0 {:min 1 :max 50})]
+
+  :blur
+  [(gen-filter-param :range "Kernal Width" "1" 0 {:min 1 :max 100})
+   (gen-filter-param :range "Kernal Height" "1" 0 {:min 1 :max 100})]})
 
 
 filter-params
 
 
 (def transformation-labels
-  {:canny          "Canny Edge Finder"
-   :gaussian-blur  "Gaussian Blur"
-   :bilateral-filter "Bilateral Filter"})
+  {:canny            "Canny Edge Finder"
+   :gaussian-blur    "Gaussian Blur"
+   :bilateral-filter "Bilateral Filter"
+   :sobel            "Sobel"
+   :median-blur      "Median Blur"
+   :box-filter       "Box Filter"
+   :blur             "Standard Blur"})
 
 
 (defn generate-default-params [param-list selection]
